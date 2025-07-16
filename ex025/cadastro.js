01a078f0454e39391272f9fe1085e684c4553f82
joicealvesdev <joicealvesrt18@gmail.com>
2025-07-16 10:44:59 -0300

    new

--- ex025/cadastro.js
+++ ex025/cadastro.js
@@ -1,3 +1,13 @@
-site não disponivel, aprenda Javascrip e resolva voce mesmo
 
+function capturarDados(event) {
+    event.preventDefault()
+
+     const name = document.getElementById['name'].value
+
+     const senha = document.getElementById['senha'].value
+
+     console.log('Usuário:', name)
+     console.log('Senha:', senha)
+    
+}
 
